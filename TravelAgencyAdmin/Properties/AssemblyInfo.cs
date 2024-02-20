93581cddd8a7313d7e13b241638840ce43fa3120
Libor Svoboda <libor.svoboda@kliknezde.cz>
2024-02-20 16:09:06 +0100

    before app upd

--- TravelAgencyAdmin/Properties/AssemblyInfo.cs
+++ TravelAgencyAdmin/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Windows;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.6.56.2259")]
-[assembly: AssemblyFileVersion("1.6.56.2259")]
+[assembly: AssemblyVersion("1.6.56.2260")]
+[assembly: AssemblyFileVersion("1.6.56.2260")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
